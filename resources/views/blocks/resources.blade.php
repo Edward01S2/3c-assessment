ed5e9edc73dc1a6c47dd38706624339604e472f4
edward01s2 <edward.01s2@gmail.com>
2021-04-27 14:14:02 -0500

    Rebuild

--- resources/views/blocks/resources.blade.php
+++ resources/views/blocks/resources.blade.php
@@ -65,7 +65,7 @@
                 </div>
                 <div class="pt-2">
                   <a href="{!! $item['link']['url'] !!}" target="_blank" class="flex items-center justify-center">
-                    <span class="mr-2">{!! $item['link']['title'] !!}</span>
+                    <span class="mr-2">{{ ($item['link']['title']) ? $item['link']['title'] : 'Learn More' }}</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
