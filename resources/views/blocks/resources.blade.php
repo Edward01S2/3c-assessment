d51d54884c876cdddd18ae8cae6846882c8f1138
edward01s2 <edward.01s2@gmail.com>
2021-07-22 13:22:17 -0500

    Add if check for logo

--- resources/views/blocks/resources.blade.php
+++ resources/views/blocks/resources.blade.php
@@ -17,63 +17,70 @@
         </select>
       </div>
       @php($count = count($posts))
-      <div class="grid grid-rows-{!! $count !!} gap-6 auto-rows-fr sm:grid-rows-none sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
+      <div
+        class="grid grid-rows-{!! $count !!} gap-6 auto-rows-fr sm:grid-rows-none sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
         {{-- @dump($posts) --}}
         @foreach($posts as $item)
-          @if($item['uni'])
-            <div class="flex flex-col bg-white shadow-lg">
-              <div class="h-56 img-shadow sm:h-64">
-                <img class="object-cover object-center w-full h-full" src="{!! $item['image'] !!}" alt="">
-              </div>
-              <div class="flex-grow p-4 pb-0">
-                @isset($item['tags'])
-                  <div class="text-sm uppercase text-c-blue-100">{!! $item['tags'] !!}</div>
-                @endisset
-                <h3 class="my-2 text-xl font-semibold text-c-blue-400 xl:text-2xl">{!! $item['title'] !!}</h3>
-                <p class="text-c-blue-200">{!! $item['excerpt'] !!}</p>
-              </div>
-              <div class="p-4">
-                <div class="border-b border-[#A7D2F1} pb-2">
-                  <img class="w-auto h-8" src="{!! $item['logo']['url'] !!}" alt="">
-                </div>
-                <div class="pt-2">
-                  <button class="flex items-center justify-center w-full focus:outline-none" @click="hide_pop = false">
-                    <span class="mr-2">{{ ($item['link']['title']) ? $item['link']['title'] : 'Learn More' }}</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-            </div> 
-          @else
-            <div class="flex flex-col bg-white shadow-lg">
-              <div class="h-56 img-shadow sm:h-64">
-                <img class="object-cover object-center w-full h-full" src="{!! $item['image'] !!}" alt="">
-              </div>
-              <div class="flex-grow p-4 pb-0">
-                @isset($item['tags'])
-                  <div class="text-sm uppercase text-c-blue-100">{!! $item['tags'] !!}</div>
-                @endisset
-                <h3 class="my-2 text-xl font-semibold text-c-blue-400 xl:text-2xl">{!! $item['title'] !!}</h3>
-                <p class="text-c-blue-200">{!! $item['excerpt'] !!}</p>
-              </div>
-              <div class="p-4">
-                <div class="border-b border-[#A7D2F1} pb-2">
-                  <img class="w-auto h-8" src="{!! $item['logo']['url'] !!}" alt="">
-                </div>
-                <div class="pt-2">
-                  <a href="{!! $item['link']['url'] !!}" target="_blank" class="flex items-center justify-center">
-                    <span class="mr-2">{{ ($item['link']['title']) ? $item['link']['title'] : 'Learn More' }}</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                  </a>
-                </div>
-              </div>
+        @if($item['uni'])
+        <div class="flex flex-col bg-white shadow-lg">
+          <div class="h-56 img-shadow sm:h-64">
+            <img class="object-cover object-center w-full h-full" src="{!! $item['image'] !!}" alt="">
+          </div>
+          <div class="flex-grow p-4 pb-0">
+            @isset($item['tags'])
+            <div class="text-sm uppercase text-c-blue-100">{!! $item['tags'] !!}</div>
+            @endisset
+            <h3 class="my-2 text-xl font-semibold text-c-blue-400 xl:text-2xl">{!! $item['title'] !!}</h3>
+            <p class="text-c-blue-200">{!! $item['excerpt'] !!}</p>
+          </div>
+          <div class="p-4">
+            <div class="border-b border-[#A7D2F1} pb-2">
+              @if($item['logo'])
+              <img class="w-auto h-8" src="{!! $item['logo']['url'] !!}" alt="">
+              @endif
             </div>
-          @endif
+            <div class="pt-2">
+              <button class="flex items-center justify-center w-full focus:outline-none" @click="hide_pop = false">
+                <span class="mr-2">{{ ($item['link']['title']) ? $item['link']['title'] : 'Learn More' }}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+        </div>
+        @else
+        <div class="flex flex-col bg-white shadow-lg">
+          <div class="h-56 img-shadow sm:h-64">
+            <img class="object-cover object-center w-full h-full" src="{!! $item['image'] !!}" alt="">
+          </div>
+          <div class="flex-grow p-4 pb-0">
+            @isset($item['tags'])
+            <div class="text-sm uppercase text-c-blue-100">{!! $item['tags'] !!}</div>
+            @endisset
+            <h3 class="my-2 text-xl font-semibold text-c-blue-400 xl:text-2xl">{!! $item['title'] !!}</h3>
+            <p class="text-c-blue-200">{!! $item['excerpt'] !!}</p>
+          </div>
+          <div class="p-4">
+            <div class="border-b border-[#A7D2F1} pb-2">
+              <img class="w-auto h-8" src="{!! $item['logo']['url'] !!}" alt="">
+            </div>
+            <div class="pt-2">
+              <a href="{!! $item['link']['url'] !!}" target="_blank" class="flex items-center justify-center">
+                <span class="mr-2">{{ ($item['link']['title']) ? $item['link']['title'] : 'Learn More' }}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        @endif
         @endforeach
       </div>
     </div>
